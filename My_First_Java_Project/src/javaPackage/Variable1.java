860a7ac7bcc8dd085761bcff8cae5f7e0360bd00
sbmuthe <102505217+sbmuthe@users.noreply.github.com>
2024-07-25 18:03:06 +0530

    Update Variable1.java
    New updates

--- My_First_Java_Project/src/javaPackage/Variable1.java
+++ My_First_Java_Project/src/javaPackage/Variable1.java
@@ -5,6 +5,7 @@ public class Variable1 {
 	public static void main(String[] args) {
 		int data = 50;
 		int meta = 50;
+		INT SET = 78;
 		System.out.println(data);
 		System.out.println(data+meta);
 		boolean sata= data>=meta;
